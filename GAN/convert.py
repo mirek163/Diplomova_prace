fa72411b725faabbd570b9b4a9138b827a7d3a54
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-11-05 16:48:11 +0100

    Variace pro pohyb okna

--- GAN/convert.py
+++ GAN/convert.py
@@ -1,13 +1,20 @@
-import trimesh
+import bpy
+import random
 
+building = bpy.data.objects.get("Building")
+window = bpy.data.objects.get("Window")
 
-def convert_obj_to_ply(obj_file, ply_file):
-    # Load the OBJ file
-    mesh = trimesh.load(obj_file)
+if building and window:
+    building_dims = building.dimensions
+    building_location = building.location
+    buffer = 0.1
+    min_x = building_location.x - (building_dims.x/2) + (window.dimensions.x / 2) + buffer 
+    max_x = building_location.x + (building_dims.x/2) - (window.dimensions.x / 2) - buffer 
+    min_z = building_location.z - (building_dims.z/2) + (window.dimensions.z / 2) + buffer 
+    max_z = building_location.z + (building_dims.z/2) - (window.dimensions.z / 2) - buffer 
+    window.location.x = random.uniform(min_x, max_x)
+    window.location.z = random.uniform(min_z, max_z)
+    window.location.y = building_location.y  
 
-    # Export as PLY
-    mesh.export(ply_file)
-
-
-# Usage
-convert_obj_to_ply('output.obj', 'output.ply')
+else:
+    print("Object not found.")
